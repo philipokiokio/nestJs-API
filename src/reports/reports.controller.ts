5fe011aaff5c41e11ca463b747ebc4f139ed44da
Philip Okiokio <philipokiokio@gmail.com>
2022-07-09 14:43:58 +0100

    looking forward to the server running,

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,13 +1,13 @@
 import { Controller , Post, Body, UseGuards, Patch,Param, Query, Get } from '@nestjs/common';
 import { CreateReportDto } from './dto/createReportDto';
-import { AuthGuard } from '../../src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 import { ReportsService } from './reports.service';
-import { CurrentUser } from '../../src/users/decorators/current-user.decorator';
-import { User } from '../../src/users/user.entity';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 import { ReportDto } from './dto/report.dto';
-import { Serialize } from '../../src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApprovedReportDto } from './dto/approvedReportDto';
-import { AdminGuard } from '../../src/guards/admin.guard';
+import { AdminGuard } from '../guards/admin.guard';
 import { GetEstimateDto } from './dto/getEstimate.dto';
 
 
